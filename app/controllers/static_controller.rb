c2ddbbdbb74db18c1c4f90aa63dfd79123b02fbc
devWRM <mcwen@comcast.net>
2020-08-12 11:32:27 -0400

    Done.

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,12 @@
+
+class StaticController < ApplicationController
+
+    
+    def hello_world
+        
+        # A controller with no control!
+        # render :"static/hello_world"
+    end
+
+end
+
